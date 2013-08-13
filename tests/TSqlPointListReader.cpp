963dea5ef10a7e0973d512da55a341efc418f29c
Nikita Ostrenkov <xfakehopex@gmail.com>
2013-08-13 13:44:51 +0400

    Обновлены тесты TSqlPointListReader

--- tests/TSqlPointListReader.cpp
+++ tests/TSqlPointListReader.cpp
@@ -6,42 +6,258 @@ TSqlPointListReader::TSqlPointListReader()
 
 void TSqlPointListReader::TestWriteRead_data()
 {
-    QTest::addColumn<ID>("item");
-    QTest::addColumn<PointList>("points");
+    QTest::addColumn<IDList>("items");
+    QTest::addColumn<SequencePointList>("points");
     QTest::addColumn<IDList>("allItems");
+    QTest::addColumn<SequencePointList>("allPoints");
 
-    QTest::newRow("empty-point-list") << ID("id1")
-                                      << PointList()
-                                      << IDList();
 
-    QTest::newRow("single-point-list") << ID("id1")
-                                       << (PointList() << Point(0.0))
-                                       << (IDList() << ID("id1"));
 
-    QTest::newRow("two-point-list") << ID("id1")
-                                    << (PointList() << Point(1.0) << Point(2.0))
-                                    << (IDList() << ID("id1"));
 
-    QTest::newRow("ten-point-list") << ID("id1")
-                                    << (PointList()
-                                        << Point(1.1)
-                                        << Point(2.2)                                           << Point(0.0)
-                                        << Point(4.4)
-                                        << Point(8.8)
-                                        << Point(16.16)
-                                        << Point(32.32)
-                                        << Point(64.64)
-                                        << Point(128.128)
-                                        << Point(256.256)
-                                        << Point(512.512))
-                                    << (IDList() << ID("id1"));;
+    QTest::newRow("empty") << IDList()
+                           << SequencePointList()
+                           << IDList()
+                           << SequencePointList();
+
+    QTest::newRow("one-item-with-one-point")
+            << (IDList()
+                << ID("id1")
+                )
+            << (SequencePointList()
+                << (PointList()
+                    << Point(1.0)
+                    )
+                )
+            << (IDList()
+                << ID("id1")
+                )
+            << (SequencePointList()
+                << (PointList()
+                    << Point(1.0)
+                    )
+                );
+
+
+    QTest::newRow("one-item-with-two-point")
+            << (IDList()
+                << ID("id1")
+                )
+            << (SequencePointList()
+                << (PointList()
+                    << Point(1.0)
+                    << Point(2.0)
+                    )
+                )
+            << (IDList()
+                << ID("id1")
+                )
+            << (SequencePointList()
+                << (PointList()
+                    << Point(1.0)
+                    << Point(2.0)
+                    )
+                );
+
+    QTest::newRow("one-item-with-ten-point")
+            << (IDList()
+                << ID("id1")
+                )
+            << (SequencePointList()
+                << (PointList()
+                    << Point(1.1)
+                    << Point(2.2)
+                    << Point(4.4)
+                    << Point(8.8)
+                    << Point(16.16)
+                    << Point(32.32)
+                    << Point(64.64)
+                    << Point(128.128)
+                    << Point(256.256)
+                    << Point(512.512)
+                    )
+                )
+            << (IDList()
+                << ID("id1")
+                )
+            << (SequencePointList()
+                << (PointList()
+                    << Point(1.1)
+                    << Point(2.2)
+                    << Point(4.4)
+                    << Point(8.8)
+                    << Point(16.16)
+                    << Point(32.32)
+                    << Point(64.64)
+                    << Point(128.128)
+                    << Point(256.256)
+                    << Point(512.512)
+                    )
+                );
+
+    QTest::newRow("two-item-with-one-point")
+            << (IDList()
+                << ID("id1")
+                << ID("id2")
+                )
+            << (SequencePointList()
+                << (PointList()
+                    << Point(4.9)
+                    << Point(6.8)
+                    << Point(11.0)
+                    )
+                << (PointList()
+                    << Point(14.01)
+                    << Point(12.3)
+                    << Point(3.3)
+                    << Point(8.1)
+                    )
+                )
+            << (IDList()
+                << ID("id1")
+                << ID("id2")
+                )
+            << (SequencePointList()
+                << (PointList()
+                    << Point(4.9)
+                    << Point(6.8)
+                    << Point(11.0)
+                    )
+                << (PointList()
+                    << Point(14.01)
+                    << Point(12.3)
+                    << Point(3.3)
+                    << Point(8.1)
+                    )
+                );
+
+    QTest::newRow("three-item-with-one-point")
+            << (IDList()
+                << ID("id1")
+                << ID("id2")
+                << ID("id3")
+                )
+            << (SequencePointList()
+                << (PointList()
+                    << Point(41.29)
+                    << Point(4.3)
+                    )
+                << (PointList()
+                    << Point(1.25)
+                    << Point(3.4)
+                    << Point(8.34)
+                    << Point(15.6)
+                    << Point(38.009)
+                    )
+                << (PointList()
+                    << Point(2.44)
+                    << Point(5.8)
+                    )
+                )
+            << (IDList()
+                << ID("id1")
+                << ID("id2")
+                << ID("id3")
+                )
+            << (SequencePointList()
+                << (PointList()
+                    << Point(41.29)
+                    << Point(4.3)
+                    )
+                << (PointList()
+                    << Point(1.25)
+                    << Point(3.4)
+                    << Point(8.34)
+                    << Point(15.6)
+                    << Point(38.009)
+                    )
+                << (PointList()
+                    << Point(2.44)
+                    << Point(5.8)
+                    )
+                );
+
+
+    QTest::newRow("two-duplicate-items-with-one-point")
+            << (IDList()
+                << ID("id1")
+                << ID("id1")
+                << ID("id2")
+                )
+            << (SequencePointList()
+                << (PointList()
+                    << Point(1.0)
+                    )
+                << (PointList()
+                    << Point(2.0)
+                    )
+                << (PointList()
+                    << Point(2.5)
+                    )
+                )
+            << (IDList()
+                << ID("id1")
+                << ID("id2")
+                )
+            << (SequencePointList()
+                << (PointList()
+                    << Point(1.0)
+                    )
+                << (PointList()
+                    << Point(2.5)
+                    )
+                );
+
+    QTest::newRow("two-doubled-duplicate-items-with-one-point")
+            << (IDList()
+                << ID("id1")
+                << ID("id1")
+                << ID("id2")
+                << ID("id3")
+                << ID("id2")
+                )
+            << (SequencePointList()
+                << (PointList()
+                    << Point(1.3)
+                    )
+                << (PointList()
+                    << Point(6.7)
+                    )
+                << (PointList()
+                    << Point(12.1)
+                    )
+                << (PointList()
+                    << Point(14.58)
+                    )
+                << (PointList()
+                    << Point(3.33)
+                    )
+
+                )
+            << (IDList()
+                << ID("id1")
+                << ID("id2")
+                << ID("id3")
+                )
+            << (SequencePointList()
+                << (PointList()
+                    << Point(1.3)
+                    )
+                << (PointList()
+                    << Point(12.1)
+                    )
+                << (PointList()
+                    << Point(14.58)
+                    )
+                );
+
 }
 
 void TSqlPointListReader::TestWriteRead()
 {
-    QFETCH(ID, item);
-    QFETCH(PointList, points);
+    QFETCH(IDList, items);
+    QFETCH(SequencePointList, points);
     QFETCH(IDList, allItems);
+    QFETCH(SequencePointList, allPoints);
 
     const QString dataBaseName = QString(QTest::currentDataTag()) + "TestWriteRead.db";
 
@@ -54,8 +270,14 @@ void TSqlPointListReader::TestWriteRead()
     }
 
     SqlPointListWriter writer(dataBaseName);
+    {
+        int index = 0;
+        foreach (const ID& item, items) {
+            writer.write(item, points[index]);
+            index++;
+        }
+    }
 
-    writer.write(item, points);
 
     QVERIFY(QFile::exists(dataBaseName));
 
@@ -66,8 +288,14 @@ void TSqlPointListReader::TestWriteRead()
 
     QCOMPARE(actualAllItems, expectedAllItems);
 
-    const PointList actualPoints = reader.read(item);
-    const PointList expectedPoints = points;
+    SequencePointList seqFromDataBase;
+    foreach (const ID& item, actualAllItems)
+    {
+        seqFromDataBase.append(reader.read(item));
+    }
+
+    const SequencePointList actualPoints = seqFromDataBase;
+    const SequencePointList expectedPoints = allPoints;
 
     QCOMPARE(actualPoints, expectedPoints);
 }
